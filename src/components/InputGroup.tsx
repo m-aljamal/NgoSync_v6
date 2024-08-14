7a9427e2cba17f28c09868752572ebc093f04410
m-jamal <mohammadjamol@gmail.com>
2024-08-14 16:58:17 +0300

    update project

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -1,12 +1,22 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { useMediaQuery } from "@/hooks/use-media-query"
+
 export default function InputGroup({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const isDesktop = useMediaQuery("(min-width: 640px)")
 
-    
   return (
-    <div className="col-span-2 grid grid-cols-2 gap-x-2 gap-y-6">
+    <div
+      className={cn(
+        `col-span-2 grid grid-cols-2 gap-x-2 gap-y-4`,
+        isDesktop ? "grid-cols-2" : "grid-cols-1 px-5"
+      )}
+    >
       {children}
     </div>
   )
