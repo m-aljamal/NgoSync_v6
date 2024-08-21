83305e1b87479f0838dfa1f1c6344d243c58912f
m-aljamal <mohammadjamol@gmail.com>
2024-08-21 12:53:58 +0300

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,6 +15,7 @@ import * as schema from "./schema"
 // export const db = drizzle(client, { schema })
 // const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
 
+
 const client = postgres(env.DATABASE_URL)
 export const db = drizzle(client, { schema })
 
