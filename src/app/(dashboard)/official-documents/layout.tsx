0fb51a717773d7af30068b81a00cd1482048b4a4
m-aljamal <mohammadjamol@gmail.com>
2024-11-14 12:22:38 +0300

    implment official

---
+++ src/app/(dashboard)/official-documents/layout.tsx
@@ -0,0 +1,47 @@
+ import { type SidebarLinks } from "@/types"
+
+import AppSidebar from "@/components/app-sidebar"
+ 
+interface Props {
+  children: React.ReactNode
+}
+
+export default async function layout({ children }: Props) {
+  const links: SidebarLinks = [
+    {
+      groupName: "التوثيق الرسمي",
+      items: [
+        {
+          title: "البيانات",
+          icon: "FileDigit",
+          href: `/official-documents/overview`,
+        },
+        {
+          title: "الحوالات",
+          icon: "Import",
+          href: `/official-documents/transfers`,
+        },
+        {
+          title: "المصروفات",
+          icon: "ArrowUpRight",
+          href: `/official-documents/outcome`,
+        },
+        {
+          href: `/official-documents/donations`,
+          title: "التبرعات",
+          icon: "HandCoins",
+        },
+      ],
+    },
+  ]
+
+  const breadcrumbs = [
+    { title: "التوثيق الرسمي", href: "/official-documents/overview" },
+  ]
+
+  return (
+    <AppSidebar links={links} breadcrumbs={breadcrumbs}>
+      <main>{children}</main>
+    </AppSidebar>
+  )
+}
