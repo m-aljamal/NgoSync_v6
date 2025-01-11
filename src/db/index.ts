9f5a5e3503de2faeb172ee0be83f11518837dbba
m-aljamal <mohammadjamol@gmail.com>
2025-01-11 12:41:21 +0300

    test upload

--- src/db/index.ts
+++ src/db/index.ts
@@ -25,6 +25,7 @@ import { env } from "@/env.js"
 import { Pool } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-serverless"
 
+
 // import { neon } from "@neondatabase/serverless"
 // import { drizzle } from "drizzle-orm/postgres-js"
 // import postgres from "postgres"
